3f3b562d3747bb108809bb841b9c21e060ab8e71
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-01-25 14:25:37 +1100

    Add file-or-url context

--- fullcontext/files.py
+++ fullcontext/files.py
@@ -56,3 +56,29 @@ def download(url : str, **kwargs) -> str:
         yield f.name
     finally:
         os.remove(f.name)
+
+
+# file-or-url code below adapted from scikit-image under the BSD license.
+URL_REGEX = re.compile(r'http://|https://|ftp://|file://|file:\\')
+
+
+def is_url(filename):
+    """Return True if string is an http or ftp path."""
+    return (isinstance(filename, six.string_types) and
+            URL_REGEX.match(filename) is not None)
+
+
+@contextmanager
+def file_or_url(resource_name):
+    """Yield name of file from the given resource (i.e. file or url)."""
+    if is_url(resource_name):
+        _, ext = os.path.splitext(resource_name)
+        with tempfile.NamedTemporaryFile(delete=False, suffix=ext) as f:
+            u = urlopen(resource_name)
+            f.write(u.read())
+        try:
+            yield f.name
+        finally:
+            os.remove(f.name)
+    else:
+        yield resource_name
